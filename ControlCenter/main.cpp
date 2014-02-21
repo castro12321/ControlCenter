bd7ee5c10ba4006a670aa211e510443f9af32dbc
castro12321 <norbertkawinski@gmail.com>
2014-02-21 17:53:28 +0100

    Renamed ControlCenter.cpp to main.cpp

---
+++ ControlCenter/main.cpp
@@ -0,0 +1,14 @@
+// ControlCenter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "ControlCenter.h"
+
+
+int main(int argc, char *argv[])
+{
+	ControlCenter app;
+	app.run();
+	return 0;
+}
