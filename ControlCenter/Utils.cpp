6ef1cd71e68b8439eac37d7178104e4430cca6ae
castro12321 <norbertkawinski@gmail.com>
2014-03-01 17:53:03 +0100

    Copied getApplications from OpenHandler to Utils

--- ControlCenter/Utils.cpp
+++ ControlCenter/Utils.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Utils.h"
+#include <dirent.h>
 
 std::vector<std::string>& Utils::split(const std::string &s, char delim, std::vector<std::string> &elems, int limit)
 {
@@ -20,4 +21,21 @@ std::vector<std::string> Utils::split(const std::string& s, char delim, int limi
 {
 	std::vector<std::string> elems;
 	return split(s, delim, elems, limit);
+}
+
+
+std::vector<std::string> Utils::getDeclaredApplications()
+{
+	std::vector<std::string> applications;
+
+	DIR* dir = opendir("programs/");
+	dirent* ent;
+	if (dir != NULL)
+		while ((ent = readdir(dir)) != NULL)
+			applications.push_back(ent->d_name);
+	else
+		std::cout << "could not open directory" << std::endl;
+	closedir(dir);
+
+	return applications;
 }
--- ControlCenter/Utils.h
+++ ControlCenter/Utils.h
@@ -8,6 +8,9 @@ namespace Utils
 {
 	std::vector<std::string> split(const std::string& s, char delim, int limit = INT_MAX); // Splits the string into a substrings into the vector.
 	std::vector<std::string>& split(const std::string &s, char delim, std::vector<std::string> &elems, int limit = INT_MAX);
+
+	// Returns all application names declared in ./programs dir.
+	std::vector<std::string> getDeclaredApplications();
 };
 
 
