8a702710ecf59a81709947b8d815679b73b6e24b
castro12321 <norbertkawinski@gmail.com>
2014-03-01 17:28:56 +0100

    Added debugging cout's

--- ControlCenter/CommandRecognizer.cpp
+++ ControlCenter/CommandRecognizer.cpp
@@ -14,23 +14,31 @@ CommandRecognizer::~CommandRecognizer()
 
 void CommandRecognizer::fillPattern()
 {
-	patterns.push_back(std::regex("open .*"));
-	patterns.push_back(std::regex("close .*"));
+	patterns.push_back(std::regex("open *"));
+	patterns.push_back(std::regex("close *"));
 	//etc.
 }
 
 CommandHandler* CommandRecognizer::recognizeCommand(std::string sentence)
 {
+	std::cout << "Trying to recognize sentence " << sentence << "\n";
+
 	fillPattern();
 	std::smatch result;
 	for (std::regex pattern : patterns)
-		if(std::regex_search(sentence, result, pattern))
+	{
+		if (std::regex_search(sentence, result, pattern))
 		{
-			if(result[0] == "open ")
+			if (result[0] == "open ")
+			{
+				std::cout << "Recognized open command!\n";
 				return new OpenHandler();
+			}
 			//if(result[0] == "close ")
 				//return new CloseHandler(sentence);
 		}
+	}
 
+	std::cout << "Nothing found! Returning nullptr :<\n";
 	return nullptr;
 }
