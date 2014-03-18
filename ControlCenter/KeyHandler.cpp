9f702649015054cc4a49272d98fb92c1172d1da1
piotr95 <bielski.pp@gmail.com>
2014-03-18 15:48:03 +0100

    Added two methods to KeyHandler
    methods: keyUp, keyDown

--- ControlCenter/KeyHandler.cpp
+++ ControlCenter/KeyHandler.cpp
@@ -29,6 +29,36 @@ void KeyHandler::generateKey(int vk, BOOL bExtended)
 	::SendInput(1, &input, sizeof(input));
 }
 
+void KeyHandler::keyUp(int vk, BOOL bExtended)
+{
+	KEYBDINPUT kb = { 0 };
+	INPUT   input = { 0 };
+
+	if (bExtended)
+		kb.dwFlags = KEYEVENTF_EXTENDEDKEY;
+	kb.wVk = vk;
+	input.type = INPUT_KEYBOARD;
+
+	input.ki = kb;
+	::SendInput(1, &input, sizeof(input));
+}
+
+void KeyHandler::keyDown(int vk, BOOL bExtended)
+{
+	KEYBDINPUT kb = { 0 };
+	INPUT   input = { 0 };
+
+	::ZeroMemory(&kb, sizeof(KEYBDINPUT));
+	::ZeroMemory(&input, sizeof(INPUT));
+	kb.dwFlags = KEYEVENTF_KEYUP;
+	if (bExtended)
+		kb.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+
+	kb.wVk     = vk;
+	input.type = INPUT_KEYBOARD;
+	input.ki   = kb;
+	::SendInput(1, &input, sizeof(input));
+}
 
 KeyHandler::KeyHandler()
 {
--- ControlCenter/KeyHandler.h
+++ ControlCenter/KeyHandler.h
@@ -7,6 +7,8 @@ class KeyHandler : public CommandHandler
 protected:
 	KeyHandler();
 	void generateKey(int virtualKey, BOOL extended);
+	void keyUp(int virtualKey, BOOL extended);
+	void keyDown(int virtualKey, BOOL extended);
 
 public:
 	virtual ~KeyHandler();
